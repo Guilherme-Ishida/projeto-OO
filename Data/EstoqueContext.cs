850a7d8112585612013f7074451c8702c38b9364
Guilherme-Ishida <guilhermeishida1@gmail.com>
2025-03-25 20:51:47 -0300

    Add files via upload

---
+++ Data/EstoqueContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEstoque.Models;
+
+namespace ProjetoEstoque.Data
+{
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext(DbContextOptions<EstoqueContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Definir relação entre Produto e Categoria (1:N)
+            modelBuilder.Entity<Produto>()
+                .HasOne(p => p.Categoria)
+                .WithMany(c => c.Produtos)
+                .HasForeignKey(p => p.CategoriaId);
+
+        }
+    }
+}
